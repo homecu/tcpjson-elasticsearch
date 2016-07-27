954e12eee72b81e48e77f9fdc6600fd6ecf9fdb7
Justin Mayfield <tooker@gmail.com>
2016-07-27 11:43:39 -0600

    Use asyncio.create_server

--- tcpjson_elasticsearch.py
+++ tcpjson_elasticsearch.py
@@ -9,61 +9,48 @@ import shellish
 loop = asyncio.get_event_loop()
 
 
-class TCPJSONServerProtocol(object):
-
-    def connection_made(self, transport):
-        self.transport = transport
-        server = transport._server
-        self.verbose = server.verbose
-        self.es_session = server.es_session
-        self.es_url = server.es_url
-
-    def data_received(self, data):
-        log = json.loads(data.decode())
-        peer = self.transport.get_extra_info('peername')[0]
-        if 'timestamp' not in log:
-            log['timestamp'] = datetime.datetime.utcnow().isoformat()
-        log['host_ip'] = peer
-        if self.verbose:
-            shellish.vtmlprint('<b>LOG:<b>', log)
-        asyncio.ensure_future(self.relaylog(log))
-
-    def eof_received(self):
-        pass
-
-    def connection_lost(self, foo):
-        pass
-
-    async def relaylog(self, log):
-        data = json.dumps(log)
-        with aiohttp.Timeout(60):
-            async with self.es_session.post(self.es_url, data=data) as r:
-                if r.status != 201:
-                    raise Exception(await r.text())
-                if self.verbose:
-                    shellish.vtmlprint('<b>ES INDEX:</b>', await r.text())
-
-
 @shellish.autocommand
-def tcpjson_es_relay(elasticsearch_url, listen_addr='0.0.0.0', listen_port=19831,
-                     verbose=False, es_conn_limit=100):
+def tcpjson_es_relay(elasticsearch_url, listen_addr='0.0.0.0',
+                     listen_port=19831, verbose=False, es_conn_limit=100):
     """ A tcp/json server relay to elasticsearch.
 
     The URL should contain the /index/type args as per the elasticsearch API.
 
     E.g. https://elasticsearch/logs/docker
     """
-    setup = loop.create_server(TCPJSONServerProtocol, listen_addr, listen_port)
-    server = loop.run_until_complete(setup)
-    server.verbose = verbose
     conn = aiohttp.TCPConnector(limit=es_conn_limit)
-    server.es_session = aiohttp.ClientSession(loop=loop, connector=conn)
-    server.es_url = elasticsearch_url
+    es_session = aiohttp.ClientSession(loop=loop, connector=conn)
+    es_url = elasticsearch_url
+
+    @asyncio.coroutine
+    def on_data(reader, writer):
+        data = yield from reader.readline()
+        log = json.loads(data.decode())
+        addr = writer.get_extra_info('peername')[0]
+        if 'timestamp' not in log:
+            log['timestamp'] = datetime.datetime.utcnow().isoformat()
+        log['host_addr'] = addr
+        if verbose:
+            shellish.vtmlprint('<b>LOG:<b>', log)
+        asyncio.ensure_future(relaylog(log))
+
+    async def relaylog(log):
+        data = json.dumps(log)
+        with aiohttp.Timeout(60):
+            async with es_session.post(es_url, data=data) as r:
+                if r.status != 201:
+                    raise Exception(await r.text())
+                if verbose:
+                    shellish.vtmlprint('<b>ES INDEX:</b>', await r.text())
+
+    setup = asyncio.start_server(on_data, listen_addr, listen_port, loop=loop)
+    server = loop.run_until_complete(setup)
     try:
         loop.run_forever()
     except KeyboardInterrupt:
         pass
     server.close()
+    loop.run_until_complete(server.wait_closed())
     loop.close()
 
 tcpjson_es_relay()
